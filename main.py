93a2aa85239de79ab26ac9beb13f97bf4317d739
AylineT <ayline.travers@gmail.com>
2025-01-06 16:16:19 +0100

    change data csv

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import pandas as pd
 
 # from data_preprocessing import pipe_prepocessing
 
-df =pd.read_csv('spotify_tracks.csv')
+df = pd.read_csv('spotify-data.csv')
 
 app = FastAPI()
 
 # Count of songs per year
 @app.get("/")
 def read_root():
-    df = pd.read_csv('spotify_tracks.csv')
-
     countByYear = df.groupby(['year']).size().to_dict()
     test = df.head().to_dict()
 
@@ -20,8 +18,6 @@ def read_root():
 # Name and artists with parameter year
 @app.get("/year/{year}")
 def songs_by_year(year: int):
-    df = pd.read_csv('spotify-data.csv')
-
     filtered = df[df['year'] == year]
     songs = filtered[['name', 'artists']].to_dict(orient='records')
 
@@ -30,8 +26,6 @@ def songs_by_year(year: int):
 # Count songs per artist per year
 @app.get("/artist/{year}")
 def song_per_artist(year: int):
-    df = pd.read_csv('spotify-data.csv')
-
     filtered = df[df['year'] == year]
     songsPerArtist = filtered.groupby(['artists']).size().sort_values(ascending=False).to_dict()
 
@@ -40,8 +34,6 @@ def song_per_artist(year: int):
 # Acousticness per year
 @app.get("/acousticness-per-year")
 def acousticness():
-    df = pd.read_csv('spotify-data.csv')
-
     average = df.groupby('year')['acousticness'].mean().sort_values(ascending=False).to_dict()
 
     return {"average_acousticness": average}
@@ -49,8 +41,6 @@ def acousticness():
 # Positivness depending on the mode
 @app.get("/positivness-mode")
 def positivness():
-    df = pd.read_csv('spotify-data.csv')
-
     positivness = df.groupby('mode')['valence'].mean().sort_values(ascending=False).to_dict()
     mode = df.groupby(['mode']).size().to_dict()
     sum = df.describe()
